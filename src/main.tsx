98e01484387570951f330aef5c5c38adaa53c0a0
Kristyan Carvalho <kristyancarvalho@hotmail.com>
2024-08-09 18:15:35 -0300

    added: vercel speed insights for analysis.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 import "./global.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
